d89a197ae4e0026759fea5dc317e39ea10245415
Developr <golibjonabdurasulov47@gmail.com>
2026-04-02 16:58:38 +0500

    publish

--- WebAPI/obj/Release/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Release/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1435577652c6244171c599542ff9e673d3523f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24971c36eb35ba734c155f0c3ba367e87080acf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
